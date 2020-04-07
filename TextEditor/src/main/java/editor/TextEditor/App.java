a4cfa17664181cf6b1065a4a758184f4bc2cdbf5
Parikshith <pkedilay@asu.edu>
2020-04-06 21:58:55 -0700

    Project Structure

---
+++ TextEditor/src/main/java/editor/TextEditor/App.java
@@ -0,0 +1,13 @@
+package editor.TextEditor;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
